670419e96c3e3a75bb54707d221637b54ea14f25
smolbreh-wq <smolbreh@gmail.com>
2025-08-31 01:11:38 +0500

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,9 @@ def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
